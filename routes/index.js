84360c9156b863ffd1c9fe934593bf2c84071415
d <delara@me.com>
2020-06-22 20:17:58 +0800

    -made delete function to delete email stored in the db -added new button for the button

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,6 @@ router.get('/leads', index.show_leads);
 router.get('/lead/:lead_id', index.show_lead);
 router.get('/lead/edit/:lead_id', index.show_lead_edit);
 router.post('/lead/edit/:lead_id', index.lead_edit);
+router.post('/lead/delete/:lead_id', index.lead_delete);
 
 module.exports = router;
