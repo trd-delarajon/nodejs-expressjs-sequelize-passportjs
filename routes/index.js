e2f56bd57a1ac2eea12f03ff4a137ac2829a67bb
d <delara@me.com>
2020-07-06 19:08:37 +0800

    -created login and signup page -created routes and controller for login and signup -created model for user -created migration for user -fixes some issue in index

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 let index = require('../Controllers/index');
+let user = require('../Controllers/user');
 
 /* GET home page. */
 router.get('/', index.index);
@@ -12,5 +13,9 @@ router.get('/lead/edit/:lead_id', index.show_lead_edit);
 router.post('/lead/edit/:lead_id', index.lead_edit);
 router.post('/lead/delete/:lead_id', index.lead_delete);
 router.post('/lead/delete-ajax/:lead_id', index.lead_delete_ajax);
+
+
+router.get('/login', user.show_login);
+router.get('/signup', user.show_signup);
 
 module.exports = router;
